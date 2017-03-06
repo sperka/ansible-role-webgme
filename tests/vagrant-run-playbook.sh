d41dae162a0b6a486a5de3010908ec9136b0dcf1
Gergely Varga <spherule@gmail.com>
2017-03-06 16:09:00 -0600

    initial implementation; tasks added; needs testing

---
+++ tests/vagrant-run-playbook.sh
@@ -0,0 +1,22 @@
+# run this script from the __ROOT__ path of this role
+# e.g.:
+# $> vagrant up
+# $> /bin/bash tests/vagrant-run-playbook.sh
+
+# create a temporary cfg
+echo "Create temporary ansible.cfg"
+cp tests/vagrant-ansible.cfg ./ansible.cfg
+
+# make sure dependencies are present
+echo "Installing dependencies from tests/requirements.yml"
+ansible-galaxy install -r tests/requirements.yml -p tests/dependencies
+
+# run the example playbook
+ansible-playbook tests/example-playbook.yml
+
+# remove temporary files
+echo "Removing temporary ansible.cfg"
+rm ./ansible.cfg
+
+echo "Removing ./tests/dependencies"
+rm -rf ./tests/dependencies
